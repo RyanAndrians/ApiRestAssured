db91cee008d5cdca3f552a3c4b4d559b3261ed92
RyanAndrians <76506912+RyanAndrians@users.noreply.github.com>
2022-09-24 20:00:59 +0700

    Add files via upload

--- ApiRestAssured/src/test/java/Core/Engine/Base.java
+++ ApiRestAssured/src/test/java/Core/Engine/Base.java
@@ -11,25 +11,23 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class Base {
-    public static String getResponseBody(String numberPage) {
-        given().when().get
-                ("https://api.punkapi.com/v2/beers?page=2&per_page="+numberPage)
-                .then().log().all();
-        return numberPage;
+    static String urlApi = "https://api.punkapi.com/v2/beers?page=2&per_page=";
+    static String urlCheckScheme = "https://api.punkapi.com/v2/beers";
+    public static void getResponseBody(String numberPage) {
+        given().when().get(urlApi+numberPage).then().statusCode(HttpStatus.SC_OK).extract().response();
     }
 
     public static void assertResponseBody(String numberPage){
-        get("https://api.punkapi.com/v2/beers?page=2&per_page="+numberPage).then()
+        get(urlApi+numberPage).then()
                 .statusCode(HttpStatus.SC_OK)
                 .assertThat()
                 .body("size()", is(Integer.parseInt(numberPage)));
     }
 
     public static void checkSchema(){
-        RestAssured.baseURI = "https://api.punkapi.com/v2/beers";
+        RestAssured.baseURI = urlCheckScheme ;
         given()
                 .when().get()
-                //verify JSON Schema
                 .then().assertThat()
                 .body(JsonSchemaValidator.
                         matchesJsonSchema(new File("src/main/java/Core/Schema/Schema.json")));
